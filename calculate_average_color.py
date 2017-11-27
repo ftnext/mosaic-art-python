59ccd70195b63c470f54956bee23e7d2872fd0fc
nikkie <takuyafjp@gmail.com>
2017-11-28 08:15:05 +0900

    画像の平均色算出
    * 指定されたフォルダの中の画像すべての平均色を求め、CSVファイルに出力する
    
    * TODO：一部の画像を対象に動作することを優先したため、関数にまとめるのが後回しになっている
    * TODO：すべての素材画像について実行できるように変更する
    * TODO：CSV出力先のフォルダをproduct/以下になるように変更する
    * TODO：MySQLを使うように変更する

---
+++ calculate_average_color.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+from PIL import Image
+
+
+data_list = []
+for image_name in os.listdir('image/euph_part_icon'):
+    if not image_name.endswith('.png'):
+        continue
+    im = Image.open('image/euph_part_icon/'+image_name)
+    im_width, im_height = im.size
+    red = 0
+    green = 0
+    blue = 0
+    for x in range(0, im_width):
+        for y in range(0, im_height):
+            rgba = im.getpixel((x, y))
+            red   += rgba[0]
+            green += rgba[1]
+            blue  += rgba[2]
+    red   = int(red   / (im_width*im_height))
+    green = int(green / (im_width*im_height))
+    blue  = int(blue  / (im_width*im_height))
+    data_list.append([image_name, red, green, blue])
+
+with open('average_color.csv', 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerows(data_list)
