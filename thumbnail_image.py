003dec6acaf1b091071668b1a5192aa09d3006b3
nikkie <takuyafjp@gmail.com>
2018-01-04 20:46:46 +0900

    素材画像縮小をまとめて行うプログラム

---
+++ thumbnail_image.py
@@ -0,0 +1,13 @@
+import os
+
+from PIL import Image
+
+
+THUMBNAIL_ONE_SIDE = 40
+
+for image_name in os.listdir('image/euph_part_icon'):
+    if not image_name.endswith('.png'):
+        continue
+    im = Image.open('image/euph_part_icon/'+image_name)
+    im.thumbnail((THUMBNAIL_ONE_SIDE, THUMBNAIL_ONE_SIDE))
+    im.save('material/'+image_name)
