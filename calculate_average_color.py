a0e1e14afa3b7e3a4980062f6045f6bd97cf1fef
nikkie <takuyafjp@gmail.com>
2018-01-04 22:52:02 +0900

    素材画像の平均色を求める前処理にもstatisticesを導入
    statistics導入前（平均の手計算）と比べてほとんどの画像のRGBの少なくとも1つで平均色の値が変化した。
    原因は不明。
    標準ライブラリstatisticsの結果を採用することにする。

--- calculate_average_color.py
+++ calculate_average_color.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import statistics as st
 
 from PIL import Image
 
@@ -10,18 +11,18 @@ for image_name in os.listdir('image/euph_part_icon'):
         continue
     im = Image.open('image/euph_part_icon/'+image_name)
     im_width, im_height = im.size
-    red = 0
-    green = 0
-    blue = 0
+    red_values   = []
+    green_values = []
+    blue_values  = []
     for x in range(0, im_width):
         for y in range(0, im_height):
             rgba = im.getpixel((x, y))
-            red   += rgba[0]
-            green += rgba[1]
-            blue  += rgba[2]
-    red   = round(red   / (im_width*im_height))
-    green = round(green / (im_width*im_height))
-    blue  = round(blue  / (im_width*im_height))
+            red_values.append(rgba[0])
+            green_values.append(rgba[1])
+            blue_values.append(rgba[2])
+    red   = round(st.mean(red_values))
+    green = round(st.mean(green_values))
+    blue  = round(st.mean(blue_values))
     data_list.append([image_name, red, green, blue])
 
 with open('average_color.csv', 'w', newline='') as csv_file:
