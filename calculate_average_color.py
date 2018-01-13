bcb460c9c2368b0e0f2d5cccd556d731526e82c5
Takuya Futatsugi <takuya.futatsugi@isana.net>
2018-01-13 12:15:53 +0900

    共通化した平均色計算処理の利用（素材画像平均色算出時）
    モザイクアート作成前の各素材画像の平均色算出処理を
    モジュール化した平均色算出処理に変更。
    平均色を保存した出力ファイルに変更がないことをdiffで確認した。

--- calculate_average_color.py
+++ calculate_average_color.py
@@ -1,8 +1,9 @@
 import os
 import csv
-import statistics as st
 
 from PIL import Image
+
+from mosaic_art import calc
 
 
 data_list = []
@@ -11,18 +12,7 @@ for image_name in os.listdir('image/euph_part_icon'):
         continue
     im = Image.open('image/euph_part_icon/'+image_name)
     im_width, im_height = im.size
-    red_values   = []
-    green_values = []
-    blue_values  = []
-    for x in range(0, im_width):
-        for y in range(0, im_height):
-            rgba = im.getpixel((x, y))
-            red_values.append(rgba[0])
-            green_values.append(rgba[1])
-            blue_values.append(rgba[2])
-    red   = round(st.mean(red_values))
-    green = round(st.mean(green_values))
-    blue  = round(st.mean(blue_values))
+    red, green, blue = calc.average_color_in_range(im, 0, 0, im_width, im_height)
     data_list.append([image_name, red, green, blue])
 
 with open('average_color.csv', 'w', newline='') as csv_file:
