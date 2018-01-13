5e9bd9c5eb8520dacc0c7ab647dd06c9e2997f1c
Takuya Futatsugi <takuya.futatsugi@isana.net>
2018-01-13 14:35:13 +0900

    指定した範囲の画像から色の最頻値を計算するメソッドを追加
    statistic.modeを用いて色の最頻値を求める
    
    * 変数名icon_imは微妙だが、すでに実装している箇所と統一する可能性があるので、そのままにしている。
    * statistics.mode()でエラーが返る場合は考慮していない。

--- mosaic_art/calc.py
+++ mosaic_art/calc.py
@@ -35,3 +35,36 @@ def average_color_in_range(icon_im, left, top, right, bottom):
     green = round(st.mean(green_values))
     blue  = round(st.mean(blue_values))
     return (red, green, blue)
+
+def mode_color_in_range(icon_im, left, top, right, bottom):
+    """Returns the mode color of pixels in a given region
+
+    Args:
+        icon_im: Image object of Pillow
+        left: X coordinate of the upper left point of the given region
+        top:  Y coordinate of the upper left point of the given region
+            NOTICE: (left, top) is included in the given region
+        right:  X coordinate of the lower right point of the given area
+        bottom: Y coordinate of the lower right point of the given area
+            NOTICE: (right, bottom) is NOT included in the given region
+
+    Returns:
+        A tuple which means (R, G, B)
+    """
+    if left >= right: # 引数は left < right と想定
+        print('left', left, '>= right', right)
+        return ()
+    if top >= bottom: # 引数は top < bottom と想定
+        print('bottom', bottom, '>= top', top)
+        return ()
+    color_values = []
+    for x in range(left, right):
+        for y in range(top, bottom):
+            rgba = icon_im.getpixel((x, y))
+            color_str = str(rgba[0]).zfill(3)+str(rgba[1]).zfill(3)+str(rgba[2]).zfill(3)
+            color_values.append(color_str)
+    mode_color = st.mode(color_values)
+    red   = int(mode_color[0:2])
+    green = int(mode_color[3:5])
+    blue  = int(mode_color[6:8])
+    return (red, green, blue)
