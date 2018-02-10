55c64428b8b7364241790f47d2df38da7fb4cdec
Takuya Futatsugi <takuya.futatsugi@isana.net>
2018-02-10 12:23:07 +0900

    [refactor] 出力するCSVファイル名を返すメソッドを導入
    計算のタイプに応じた出力するCSVファイル名を返すメソッドを実装。
    
    calculate_average_color.py、calculate_median_color.py、
    calculate_mode_color.pyの違いの1つである
    結果記入ファイルの違いを共通化した。

--- calculate_material_color.py
+++ calculate_material_color.py
@@ -19,13 +19,28 @@ class CalcType(Enum):
     MODE    = 3
 
 class ColorCalculator:
-    pass
+    def __init__(self, calc_type):
+        self.csv_name = ColorCalculator.color_csv_name(calc_type)
 
+    def color_csv_name(calc_type):
+        """Return output csv file name corresponding to color calculation type
+
+        Args:
+            calc_type: CalcType object
+
+        Returns:
+            output csv file name
+        """
+        if calc_type is CalcType.AVERAGE:
+            return 'average_color.csv'
+        elif calc_type is CalcType.MEDIAN:
+            return 'median_color.csv'
+        else: # calc_type is CalcType.MODE:
+            return 'mode_color.csv'
 
 def main():
-    calculator = ColorCalculator()
-    calculator.type = CalcType.AVERAGE
-    print(type(calculator))
+    calculator = ColorCalculator(CalcType.AVERAGE)
+    print(calculator.csv_name)
 
 if __name__ == '__main__':
     main()
