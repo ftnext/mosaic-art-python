955eb570b5e78927cd5eea31a0768ffc414204a3
Takuya Futatsugi <takuya.futatsugi@isana.net>
2018-02-10 12:04:37 +0900

    [refactor] 計算のタイプを指定するEnumを導入
    calculate_average_color.py、calculate_median_color.py、calculate_mode_colo
    r.pyの違いは
    計算する色がaverageかmedianかmodeかによる
    （それによってcalcモジュールから呼び出すメソッドと結果記入ファイルが変わる）
    
    そこでaverageかmedianかmodeかが指定できるEnumを導入した

--- calculate_material_color.py
+++ calculate_material_color.py
@@ -1,4 +1,5 @@
 import csv
+from enum import Enum
 import os
 
 from PIL import Image
@@ -6,12 +7,24 @@ from PIL import Image
 from mosaic_art import calc
 
 
+class CalcType(Enum):
+    """Color Caluculation Type
+
+    * calculate average color
+    * calculate median color
+    * calculate mode color
+    """
+    AVERAGE = 1
+    MEDIAN  = 2
+    MODE    = 3
+
 class ColorCalculator:
     pass
 
 
 def main():
     calculator = ColorCalculator()
+    calculator.type = CalcType.AVERAGE
     print(type(calculator))
 
 if __name__ == '__main__':
