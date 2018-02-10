36fa5fdaa4bdf179d7885bf79bbd3fc649f0f385
Takuya Futatsugi <takuya.futatsugi@isana.net>
2018-02-10 11:52:54 +0900

    [refactor] 前処理にクラスを導入するためのファイル作成
    クラスを最小構成で追加。（今後機能追加していく）
    
    calculate_average_color.py、calculate_median_color.py、calculate_mode_colo
    r.pyは
    コードがほとんど同じなので、1つのファイルにまとめる。
    （モードを平均と指定すると、素材画像の平均値が計算されるイメージ）

---
+++ calculate_material_color.py
@@ -0,0 +1,18 @@
+import csv
+import os
+
+from PIL import Image
+
+from mosaic_art import calc
+
+
+class ColorCalculator:
+    pass
+
+
+def main():
+    calculator = ColorCalculator()
+    print(type(calculator))
+
+if __name__ == '__main__':
+    main()
