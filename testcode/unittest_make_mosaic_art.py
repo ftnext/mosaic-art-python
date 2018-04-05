ce92c6f7c7695d07b65c054ae9ef3cbc99678d00
ftnext <takuyafjp+develop@gmail.com>
2018-04-05 23:38:41 +0900

    [test] モザイクアートのファイル名を返す関数のテストコード
    * モザイクアートのファイル名は現在時刻を含むので、現在時刻以外の部分をテストするケースを追加した。
    （現在時刻を年月日時分秒で表示する仕様については、Issue #21 での修正で確認した認識なので今回は用意していない）
    * ファイル名の前半部分のテスト（prefixとつく9ケース）と、拡張子のテスト（suffixとつく3ケース）を追加した。
    前者は組合せが3×3と少なかったので全て網羅することにした。
    後者はどの組合せでも同じ拡張子になるので、9通りの中から3パターンに絞って用意した。（第2引数と第3引数が一致するパターン、一致しないパターン（
    2通り））

--- testcode/unittest_make_mosaic_art.py
+++ testcode/unittest_make_mosaic_art.py
@@ -45,3 +45,110 @@ class TestColorCalculateMethod(TestCase):
         expected = calc.mode_color_in_range
         actual = make_mosaic_art.color_calculate_method('mode')
         self.assertEqual(expected, actual)
+
+class TestMosaicArtFileName(TestCase):
+    """Test of function `mosaic_art_file_name` """
+
+    def test_average_average_prefix(self):
+        """Test of file name prefix in specifying average color
+        as target representation and material representation"""
+        expected = 'test_file_mosaic_average_average_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'average', 'average')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_average_median_prefix(self):
+        """Test of file name prefix
+        in specifying average color as target representation
+        and median color as material representation"""
+        expected = 'test_file_mosaic_average_median_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'average', 'median')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_average_mode_prefix(self):
+        """Test of file name prefix
+        in specifying average color as target representation
+        and mode color as material representation"""
+        expected = 'test_file_mosaic_average_mode_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'average', 'mode')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_median_average_prefix(self):
+        """Test of file name prefix
+        in specifying median color as target representation
+        and average color as material representation"""
+        expected = 'test_file_mosaic_median_average_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'median', 'average')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_median_median_prefix(self):
+        """Test of file name prefix in specifying median color
+        as target representation and material representation"""
+        expected = 'test_file_mosaic_median_median_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'median', 'median')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_median_mode_prefix(self):
+        """Test of file name prefix
+        in specifying median color as target representation
+        and mode color as material representation"""
+        expected = 'test_file_mosaic_median_mode_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'median', 'mode')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_mode_average_prefix(self):
+        """Test of file name prefix
+        in specifying mode color as target representation
+        and average color as material representation"""
+        expected = 'test_file_mosaic_mode_average_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'mode', 'average')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_mode_median_prefix(self):
+        """Test of file name prefix
+        in specifying mode color as target representation
+        and median color as material representation"""
+        expected = 'test_file_mosaic_mode_median_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'mode', 'median')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_mode_mode_prefix(self):
+        """Test of file name prefix in specifying mode color
+        as target representation and material representation"""
+        expected = 'test_file_mosaic_mode_mode_'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'mode', 'mode')
+        self.assertTrue(actual.startswith(expected))
+
+    def test_average_average_suffix(self):
+        """Test of file name suffix in specifying average color
+        as target representation and material representation"""
+        expected = '.png'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'average', 'average')
+        self.assertTrue(actual.endswith(expected))
+
+    def test_median_mode_suffix(self):
+        """Test of file name suffix
+        in specifying median color as target representation
+        and mode color as material representation"""
+        expected = '.png'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'median', 'mode')
+        self.assertTrue(actual.endswith(expected))
+
+    def test_mode_average_suffix(self):
+        """Test of file name suffix
+        in specifying mode color as target representation
+        and average color as material representation"""
+        expected = '.png'
+        actual = make_mosaic_art.mosaic_art_file_name(
+                    'test_file.png', 'mode', 'average')
+        self.assertTrue(actual.endswith(expected))
