a3682193c4007bdd6ec915499a8257162df29992
ftnext <takuyafjp+develop@gmail.com>
2018-04-05 21:25:38 +0900

    [test] 対象画像の領域の代表色を計算する関数を返す関数のテストコード
    * 代表色の指定が平均、中央、最瀕のいずれでもない場合は、呼び出し時の引数チェックで弾く想定なので、テストケースは用意していない
    * assertEqualでfunctionの比較ができているのかがよくわかっていない。
    https://docs.python.jp/3/library/unittest.html#unittest.TestCase.assertE
    qual
    上記のドキュメントの対象にfunctionはないと思われるが、
    デフォルトで`calc.average_color_in_range`を返すようにすると1ケース通ったので比較はできているらしい。

--- testcode/unittest_make_mosaic_art.py
+++ testcode/unittest_make_mosaic_art.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from mosaic_art import calc
 import make_mosaic_art
 
 
@@ -22,4 +23,25 @@ class TestMaterialColorFileName(TestCase):
         """Test of specifying mode color as representative color"""
         expected = 'mode_color.csv'
         actual = make_mosaic_art.material_color_file_name('mode')
+        self.assertEqual(expected, actual)
+
+class TestColorCalculateMethod(TestCase):
+    """Test of function `color_calculate_method` """
+
+    def test_specify_average(self):
+        """Test of specifying average color as representative color"""
+        expected = calc.average_color_in_range
+        actual = make_mosaic_art.color_calculate_method('average')
+        self.assertEqual(expected, actual)
+
+    def test_specify_median(self):
+        """Test of specifying median color as representative color"""
+        expected = calc.median_color_in_range
+        actual = make_mosaic_art.color_calculate_method('median')
+        self.assertEqual(expected, actual)
+
+    def test_specify_mode(self):
+        """Test of specifying mode color as representative color"""
+        expected = calc.mode_color_in_range
+        actual = make_mosaic_art.color_calculate_method('mode')
         self.assertEqual(expected, actual)
